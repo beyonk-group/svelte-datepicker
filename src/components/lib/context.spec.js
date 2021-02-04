3da1c571e7e99ca34a11e8799d825927818234b6
Antony Jones <antony@beyonk.com>
2021-02-04 12:29:39 +0000

    more tests

--- src/components/lib/context.spec.js
+++ src/components/lib/context.spec.js
@@ -5,91 +5,152 @@ import { dayjs } from './date-utils.js'
 import MockDate from 'mockdate'
 import { get } from 'svelte/store'
 
-const suite = Suite('setup/range-picker/defaults')
+const defaults = Suite('setup/range-picker/defaults')
 
-const config = {
-  start: dayjs().subtract(1, 'year'),
-  end: dayjs().add(1, 'year'),
-  isRangePicker: true
-}
-let output
-
-suite.before(() => {
+defaults.before(() => {
   MockDate.set('2020-04-20')
+  defaults.ctx = {
+    config: {
+      start: dayjs().subtract(1, 'year'),
+      end: dayjs().add(1, 'year'),
+      isRangePicker: true
+    }
+  }
+  defaults.ctx.output = setup(undefined, defaults.ctx.config)
 })
 
-suite.after(() => {
+defaults.after(() => {
   MockDate.reset()
 })
 
-suite.before.each(() => {
-  output = setup(undefined, config)
-})
-
-suite('has correct start date', () => {
-  const selectedStartDate = get(output.selectedStartDate)
+defaults('has correct start date', () => {
+  const selectedStartDate = get(defaults.ctx.output.selectedStartDate)
   assert.equal(selectedStartDate.toDate(), dayjs('2020-04-20').toDate())
 })
 
-suite('has correct end date', () => {
-  const selectedEndDate = get(output.selectedEndDate)
+defaults('has correct end date', () => {
+  const selectedEndDate = get(defaults.ctx.output.selectedEndDate)
   assert.equal(selectedEndDate.toDate(), dayjs('2020-05-20').toDate())
 })
 
-suite('has two years worth of months plus one extra', () => {
-  assert.equal(output.months.length, 25)
+defaults('has two years worth of months plus one extra', () => {
+  assert.equal(defaults.ctx.output.months.length, 25)
 })
 
-suite('has correct view component', () => {
-  const component = get(output.component)
+defaults('has correct view component', () => {
+  const component = get(defaults.ctx.output.component)
   assert.equal(component, 'date-view')
 })
 
-suite('has today', () => {
-  assert.equal(output.today, dayjs().startOf('day'))
+defaults('has today', () => {
+  assert.equal(defaults.ctx.output.today, dayjs().startOf('day'))
 })
 
-suite('has correct left date', () => {
-  const leftCalendarDate = get(output.leftCalendarDate)
+defaults('has correct left date', () => {
+  const leftCalendarDate = get(defaults.ctx.output.leftCalendarDate)
   assert.equal(leftCalendarDate.toDate(), dayjs('2020-04-20').toDate())
 })
 
-suite('has correct right date', () => {
-  const rightCalendarDate = get(output.rightCalendarDate)
+defaults('has correct right date', () => {
+  const rightCalendarDate = get(defaults.ctx.output.rightCalendarDate)
   assert.equal(rightCalendarDate.toDate(), dayjs('2020-05-20').toDate())
 })
 
-suite('has passed configuration', () => {
-  assert.equal(output.config, config)
+defaults('has passed configuration', () => {
+  assert.equal(defaults.ctx.output.config, defaults.ctx.config)
 })
 
-suite('has correct open state', () => {
-  const state = get(output.isOpen)
+defaults('has correct open state', () => {
+  const state = get(defaults.ctx.output.isOpen)
   assert.not(state)
 })
 
-suite('has correct closing state', () => {
-  const state = get(output.isClosing)
+defaults('has correct closing state', () => {
+  const state = get(defaults.ctx.output.isClosing)
   assert.not(state)
 })
 
-suite('has correct highlighted day', () => {
-  const highlightedDay = get(output.highlighted)
+defaults('has correct highlighted day', () => {
+  const highlightedDay = get(defaults.ctx.output.highlighted)
   assert.equal(highlightedDay.toDate(), dayjs().toDate())
 })
 
-suite('does not have a chosen date', () => {
-  const isDateChosen = get(output.isDateChosen)
+defaults('does not have a chosen date', () => {
+  const isDateChosen = get(defaults.ctx.output.isDateChosen)
   assert.not(isDateChosen)
 })
 
-suite('has correct user state', () => {
-  const isSelectingFirstDate = get(output.isSelectingFirstDate)
+defaults('has correct user state', () => {
+  const isSelectingFirstDate = get(defaults.ctx.output.isSelectingFirstDate)
   assert.ok(isSelectingFirstDate)
 })
 
-suite('has reset function', () => {
-  assert.type(output.resetView, 'function')
+defaults('has reset function', () => {
+  assert.type(defaults.ctx.output.resetView, 'function')
 })
 
-suite.run()
+defaults.run()
+
+const sameMonth = Suite('setup/date-range/selected-dates/same-month')
+
+sameMonth.before(() => {
+  MockDate.set('2020-04-20')
+  sameMonth.ctx = {
+    config: {
+      start: dayjs().subtract(1, 'year'),
+      end: dayjs().add(1, 'year'),
+      selected: [
+        dayjs('2020-04-25'),
+        dayjs('2020-04-27')
+      ],
+      isRangePicker: true
+    }
+  }
+  sameMonth.ctx.output = setup(undefined, sameMonth.ctx.config)
+})
+
+sameMonth.after(() => {
+  MockDate.reset()
+})
+
+sameMonth('correct left-hand month is displayed', () => {
+  const date = get(sameMonth.ctx.output.leftCalendarDate)
+  assert.equal(date.toDate(), dayjs('2020-04-01').toDate())
+})
+
+sameMonth('correct right-hand month is displayed', () => {
+  const date = get(sameMonth.ctx.output.rightCalendarDate)
+  assert.equal(date.toDate(), dayjs('2020-05-01').toDate())
+})
+
+sameMonth.run()
+
+const withSelectedEndInsideRange = Suite('setup/date-range/default-dates/selected-inside-range')
+
+withSelectedEndInsideRange.before(() => {
+  MockDate.set('2021-02-04')
+  withSelectedEndInsideRange.ctx = {
+    config: {
+      start: dayjs('2021-01-11'),
+      end: dayjs('2021-04-18'),
+      isRangePicker: true
+    }
+  }
+  withSelectedEndInsideRange.ctx.output = setup(undefined, withSelectedEndInsideRange.ctx.config)
+})
+
+withSelectedEndInsideRange.after(() => {
+  MockDate.reset()
+})
+
+withSelectedEndInsideRange('left-hand month is start of default selection', () => {
+  const date = get(withSelectedEndInsideRange.ctx.output.leftCalendarDate)
+  assert.equal(date.toDate(), dayjs('2021-02-01').toDate())
+})
+
+withSelectedEndInsideRange('right hand month is next month', () => {
+  const date = get(withSelectedEndInsideRange.ctx.output.rightCalendarDate)
+  assert.equal(date.toDate(), dayjs('2021-03-01').toDate())
+})
+
+withSelectedEndInsideRange.run()
