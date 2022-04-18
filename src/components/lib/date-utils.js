f475a53f7a870fb914cbb0c45d776b368425c86a
castynet <riungemaina@gmail.com>
2022-04-18 15:25:44 +0200

    Fix: Cannot read property ‘extend’ of undefined

--- src/components/lib/date-utils.js
+++ src/components/lib/date-utils.js
@@ -1,14 +1,12 @@
-import dayjs from 'dayjs'
-import localeData from 'dayjs/plugin/localeData'
-import minMax from 'dayjs/plugin/minMax'
-import isSameOrBefore from 'dayjs/plugin/isSameOrBefore'
-import isSameOrAfter from 'dayjs/plugin/isSameOrAfter'
+import dayjs from 'dayjs';
+import localeData from 'dayjs/plugin/localeData';
+import minMax from 'dayjs/plugin/minMax';
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
+import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
 
-dayjs.extend(localeData)
-dayjs.extend(minMax)
-dayjs.extend(isSameOrBefore)
-dayjs.extend(isSameOrAfter)
+dayjs?.extend(localeData);
+dayjs?.extend(minMax);
+dayjs?.extend(isSameOrBefore);
+dayjs?.extend(isSameOrAfter);
 
-export {
-  dayjs
-}
+export { dayjs };
