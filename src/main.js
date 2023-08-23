c48161f2cfebbe8a7731aaf3ba220856168de120
Antony <antony@users.noreply.github.com>
2023-08-23 11:22:21 +0100

    upgrade svelte version

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
-import { polyfill } from 'es6-object-assign'
 import { CalendarStyle } from './calendar-style.js'
 import DatePicker from './components/DatePicker.svelte'
-polyfill()
 
 export {
   CalendarStyle,
